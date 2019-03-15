be5c00ed9c91669190930909a2de1979f8f34aff
Richie <richiekellyie@gmail.com>
2019-03-15 21:27:22 +0000

    Link to richiekelly.net in footer

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
         <p id="email">Follow us on Facebook</p>
         <a href="https://www.facebook.com/WillametteHurling/"><img src="images/fb-square-icon.png" alt="facebook icon"></a>
         <h5>willamettehurling@gmail.com</h5>
-        <h5>Website designed & implemented by Richie Kelly &copy 2019</h5>
+        <h5>Website designed & implemented by <a href="http://www.richiekelly.net">Richie Kelly</a> &copy 2019</h5>
 </footer>
